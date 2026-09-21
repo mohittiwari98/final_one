6a5f8ea6c8e40459adae40642e2d18b59abd447e
Mohit Tiwari <mohittiwari17091709@gmail.com>
2026-09-21 15:24:09 +0530

    Add advanceAttempt route to attemptRoutes

--- backend/routes/attemptRoutes.js
+++ backend/routes/attemptRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const { protect, authorize } = require('../middleware/auth');
-const { startAttempt, submitAttempt, getMyResult, getQuizAttempts } = require('../controllers/attemptController');
+const { startAttempt, advanceAttempt, submitAttempt, getMyResult, getQuizAttempts } = require('../controllers/attemptController');
 
 router.post('/start', protect, authorize('student'), startAttempt);
+router.post('/:id/advance', protect, authorize('student'), advanceAttempt);
 router.post('/:id/submit', protect, authorize('student'), submitAttempt);
 router.get('/:quizId/mine', protect, authorize('student'), getMyResult);
 router.get('/:quizId/all', protect, authorize('teacher'), getQuizAttempts);
